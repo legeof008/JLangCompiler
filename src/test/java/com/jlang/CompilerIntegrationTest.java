f7cc77ceea5fef282a200d3bb95a764fa40449c8
Patryk Ferenc <81482531+patrykferenc@users.noreply.github.com>
2024-04-09 15:19:32 +0200

    feat: add first part of cli (#12)
    * feat: add first part of cli
    
    * feat: add todo

--- src/test/java/com/jlang/CompilerIntegrationTest.java
+++ src/test/java/com/jlang/CompilerIntegrationTest.java
@@ -1,24 +1,72 @@
 package com.jlang;
 
+import com.jlang.error.ErrorLoggingBackend;
 import org.antlr.v4.runtime.CharStreams;
+import org.assertj.core.api.Assertions;
+import org.assertj.vavr.api.VavrAssertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 class CompilerIntegrationTest {
 
+    private final AssertingErrorLoggingBackend assertingErrorLoggingBackend = new AssertingErrorLoggingBackend();
     private Compiler compiler;
 
     @BeforeEach
     void setUp() {
-        compiler = new Compiler();
+        compiler = Compiler.withLogging(
+                assertingErrorLoggingBackend
+        );
     }
 
-    @Test
-    void testCompile() {
-        var output = compiler.compile(CharStreams.fromString("no to mamy x co jest intem"));
+    private static Stream<Arguments> testCompileValid() {
+        return Stream.of(
+                of("no to mamy x co jest intem"),
+                of("no to mamy x co jest intem\n")
+                // of("no  to  mamy  x  co  jest  intem\n") // TODO #14: Fix: Unexpected error reported by analysis: line 1:27 no viable alternative at input 'jestintem'
+        );
+    }
 
-        assertThat(output).isNotNull();
+    @ParameterizedTest
+    @MethodSource
+    void testCompileValid(String rawInput) {
+        var input = CharStreams.fromString(rawInput);
+        var output = compiler.compile(input);
+
+        VavrAssertions.assertThat(output)
+                .isRight();
+    }
+
+    private static Stream<Arguments> testCompileWithError() {
+        return Stream.of(
+                of("nno to mamy x co jest intem")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @Disabled("#13")
+    void testCompileWithError(String rawInput) {
+        compiler = Compiler.withDefaults(); // Do not fail if error encountered
+        var input = CharStreams.fromString(rawInput);
+        var output = compiler.compile(input);
+
+        VavrAssertions.assertThat(output)
+                .isLeft();
+    }
+
+    private static class AssertingErrorLoggingBackend implements ErrorLoggingBackend {
+
+        @Override
+        public void accept(String message) {
+            Assertions.fail("Unexpected error reported by analysis: " + message);
+        }
     }
 }
